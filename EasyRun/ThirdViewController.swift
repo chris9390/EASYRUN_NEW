24a6211a1297bcdd6b00185d0ebd7a7d43e0c4bd
Juhun Kim <johnjhkim@hanmail.net>
2018-06-21 17:51:36 +0900

    0621

--- EasyRun/ThirdViewController.swift
+++ EasyRun/ThirdViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 var savedDict : [String:String] = [:]
-var images = ["0.png", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "a.png", "b.png", "c.png", "d.png", "e.png", "f.png", "Smile Face.png", "Sad Face.png", "Angry Face.png", "x.png"]
+var images = ["0.png", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "a.png", "b.png", "c.png", "d.png", "e.png", "f.png", "Smile Face.png", "Sad Face.png", "Angry Face.png", "Check.png", "Heart.png", "Star.png"]
 var checkChange = 0
 var numberOfDict = 0
 
@@ -17,12 +17,13 @@ class ThirdViewController: UIViewController,UIPickerViewDataSource,UIPickerViewD
     
     var phonenumTextField: UITextField?
     
+    @IBOutlet weak var LabelView: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var MatchedLabel: UILabel!
     @IBAction func MatchPattern(_ sender: UIButton) {
         
-        if MatchedLabel.text != "Not Matched" {
+        if MatchedLabel.text != "X (Not Matched)" {
             let alertController = UIAlertController(title: "Do you want to overwrite?", message: nil, preferredStyle: .alert)
             
             alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: {(action) in alertController.dismiss(animated: true, completion: nil)
@@ -62,6 +63,9 @@ class ThirdViewController: UIViewController,UIPickerViewDataSource,UIPickerViewD
     
     override func viewDidLoad() {
         print(urlScheme)
+        pickerView.layer.borderWidth = 0.5
+        LabelView.layer.borderWidth = 0.5
+        LabelView.layer.borderColor = UIColor.black.cgColor
         if urlScheme == "Messages " {
             let alertController = UIAlertController(title: "Write phone number.", message: nil, preferredStyle: .alert)
             
@@ -148,7 +152,7 @@ class ThirdViewController: UIViewController,UIPickerViewDataSource,UIPickerViewD
             MatchedLabel.text = savedDict[choose]!
         }
         else{
-            MatchedLabel.text = "Not Matched"
+            MatchedLabel.text = "X (Not Matched)"
         }
     }
     
@@ -158,7 +162,7 @@ class ThirdViewController: UIViewController,UIPickerViewDataSource,UIPickerViewD
 
 struct PatternDataSource{
     let patterns = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","Angry Face",
-        "Sad Face", "Smile Face", "x"]
+        "Sad Face", "Smile Face", "Check", "Heart", "Star"]
     
     func title(for index: Int) -> String? {
         guard index < patterns.count else{ return nil }
